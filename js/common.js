f3e13538dd56e3aade746ea0d50a97a24f63ebfd
itlutsk <itlutsk@gmail.com>
2021-09-10 21:11:04 +0300

    js menu show-hide effect 2021-09-10

---
+++ js/common.js
@@ -0,0 +1,16 @@
+const sidebarHamburger = document.querySelector('.sidebar__hamburger_link');
+const sidebarClose = document.querySelector('.siebar__close');
+const sidebarMenu = document.querySelector('.sidebar');
+const sidebarClass = 'sidebar__opened';
+
+sidebarHamburger.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    sidebarMenu.classList.add(sidebarClass);
+});
+
+sidebarClose.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    sidebarMenu.classList.remove(sidebarClass);
+});
