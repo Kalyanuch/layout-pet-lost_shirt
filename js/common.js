646cdd5ccf77e8b391c7c7ba07e5b9eb88cf3ae1
Skalyan <itlutsk@gmail.com>
2021-09-10 23:54:03 +0300

    Slides layout 2021-09-10

--- js/common.js
+++ js/common.js
@@ -1,16 +1,18 @@
-const sidebarHamburger = document.querySelector('.sidebar__hamburger_link');
-const sidebarClose = document.querySelector('.siebar__close');
-const sidebarMenu = document.querySelector('.sidebar');
-const sidebarClass = 'sidebar__opened';
+(function() {
+    const sidebarHamburger = document.querySelector('.sidebar__hamburger_link');
+    const sidebarClose = document.querySelector('.siebar__close');
+    const sidebarMenu = document.querySelector('.sidebar');
+    const sidebarClass = 'sidebar__opened';
 
-sidebarHamburger.addEventListener('click', function(e) {
-    e.preventDefault();
+    sidebarHamburger.addEventListener('click', function(e) {
+        e.preventDefault();
 
-    sidebarMenu.classList.add(sidebarClass);
-});
+        sidebarMenu.classList.add(sidebarClass);
+    });
 
-sidebarClose.addEventListener('click', function(e) {
-    e.preventDefault();
+    sidebarClose.addEventListener('click', function(e) {
+        e.preventDefault();
 
-    sidebarMenu.classList.remove(sidebarClass);
-});
+        sidebarMenu.classList.remove(sidebarClass);
+    });
+}());
